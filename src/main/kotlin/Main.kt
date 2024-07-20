6431d7bea0db3e013947c16fe9b8443e8353805f
choi seoin <befine324@gmail.com>
2024-07-20 19:54:02 +0900

    pass stage

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,32 @@
+import java.io.DataOutputStream
 import java.net.ServerSocket
 
 fun main(args: Array<String>) {
-    var serverSocket = ServerSocket(6379)
+    val serverSocket = ServerSocket(6379)
 
     // Since the tester restarts your program quite often, setting SO_REUSEADDR
     // ensures that we don't run into 'Address already in use' errors
     serverSocket.reuseAddress = true
 
-    serverSocket.accept() // Wait for connection from client.
+    // syntax: PING [message]
+    serverSocket.accept().use { socket ->
+        val input = socket.getInputStream().bufferedReader()
+        val output = DataOutputStream(socket.getOutputStream())
+
+        while (true) {
+            val request = input.readLine() ?: break
+
+            val response = when {
+                request.startsWith("PING") -> {
+                    val message = request.substringAfter("PING").trim()
+                    "PONG $message"
+                }
+                else -> "ERROR unknown command"
+            }
+
+            output.writeBytes("$response\n")
+            output.flush()
+        }
+    }
     println("accepted new connection")
 }
