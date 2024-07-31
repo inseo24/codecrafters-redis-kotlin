0f16b4b97279836be092e46052a3c8856fc64c59
choi seoin <befine324@gmail.com>
2024-08-01 01:25:04 +0900

    codecrafters submit [skip ci]

--- src/main/kotlin/slave/RedisSlave.kt
+++ src/main/kotlin/slave/RedisSlave.kt
@@ -1,7 +1,13 @@
 package slave
 
-import kotlinx.coroutines.*
-import util.RedisProtocolUtils
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import util.RedisProtocolUtils.replyPing
+import util.RedisProtocolUtils.replyPsync
+import util.RedisProtocolUtils.replyReplConfCapaPsync2
+import util.RedisProtocolUtils.replyReplConfListeningPort
 import java.net.Socket
 
 class RedisSlave(private val masterHost: String, private val masterPort: Int, private val slavePort: Int) {
@@ -25,27 +31,43 @@ class RedisSlave(private val masterHost: String, private val masterPort: Int, pr
                 val reader = socket.getInputStream().bufferedReader()
 
                 // PING 전송
-                writer.write(RedisProtocolUtils.replyPing())
+                writer.write(replyPing())
                 writer.flush()
 
                 // PING 응답 확인
                 val pingResponse = reader.readLine()
                 if (pingResponse == "+PONG") {
                     // REPLCONF listening-port 전송
-                    writer.write(RedisProtocolUtils.replyReplConfListeningPort(slavePort))
+                    writer.write(replyReplConfListeningPort(slavePort))
                     writer.flush()
                     val portResponse = reader.readLine()
                     println("REPLCONF listening-port response: $portResponse")
 
                     // REPLCONF capa psync2 전송
-                    writer.write(RedisProtocolUtils.replyReplConfCapaPsync2())
+                    writer.write(replyReplConfCapaPsync2())
                     writer.flush()
                     val capaResponse = reader.readLine()
                     println("REPLCONF capa psync2 response: $capaResponse")
 
                     // PSYNC 전송
-                    writer.write(RedisProtocolUtils.replyPsync())
+                    writer.write(replyPsync())
                     writer.flush()
+                    val psyncResponse = reader.readLine()
+                    println("PSYNC response: $psyncResponse")
+
+                    // PSYNC 응답 처리
+                    if (psyncResponse.startsWith("+FULLRESYNC")) {
+                        val parts = psyncResponse.split(" ")
+                        if (parts.size == 3) {
+                            val replId = parts[1]
+                            val offset = parts[2]
+                            println("Received FULLRESYNC with REPL_ID: $replId and offset: $offset")
+                        } else {
+                            println("Unexpected FULLRESYNC response format")
+                        }
+                    } else {
+                        println("Unexpected response to PSYNC: $psyncResponse")
+                    }
                 } else {
                     println("Unexpected response to PING: $pingResponse")
                 }
